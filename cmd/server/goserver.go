6013e41642c97de04b0494cafb30602bc3269ce5
Javier Rojas <javroj95@gmail.com>
2025-04-17 04:55:00 -0500

    adding user to db working with bcrypt

--- cmd/server/goserver.go
+++ cmd/server/goserver.go
@@ -4,10 +4,12 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
 
 	"github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
 const (
@@ -83,6 +85,12 @@ type productById struct {
 	Status			string	`json:"status"`
 }
 
+type users struct {
+	Username	string `json:"username"`
+	Email 		string `json:"email"`
+	Password  	string `json:"password"`
+}
+
 func mainPageHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request){
 
@@ -114,9 +122,7 @@ func mainPageHandler(db *sql.DB) http.HandlerFunc {
 		//Check data
 		// fmt.Printf("This is inventory from db: %v\n", mainInv)
 
-
 		jData, err := json.Marshal(mainInv)
-
 
 		// fmt.Println(string(jData))
 		if err != nil {
@@ -160,10 +166,8 @@ func productByIdHandler(db *sql.DB) http.HandlerFunc {
 				if err := rows.Scan(&product.ID, &product.Product_name, &product.Category, &product.Price, &product.Sku, &product.Supplier, &product.Dimensions, & product.Status); err != nil {
 					fmt.Errorf("Error with single item in keys: %v", err)
 				}
-
 				products = append(products, product)
 
-				
 			}
 			jData, err := json.Marshal(products)
 
@@ -182,15 +186,13 @@ func productByIdHandler(db *sql.DB) http.HandlerFunc {
 			id := strings.SplitN(path, "/", 2)[0]
 	
 			rows := db.QueryRow("SELECT id, product_name, category, price, sku, supplier, dimensions, status from Inventory where id = $1", id)
-			err := rows.Scan( &product2.ID, &product2.Product_name, &product2.Category, &product2.Price, &product2.Sku, &product2.Supplier, &product2.Dimensions, & product2.Status)
-			
+			err := rows.Scan( &product2.ID, &product2.Product_name, &product2.Category, &product2.Price, &product2.Sku, &product2.Supplier, &product2.Dimensions, & product2.Status )
 			// fmt.Println(product)
-			
+
 			if err != nil {
 				fmt.Errorf("Error while getting product by ID: %v", err)
 			}
 			
-	
 			jData, err := json.Marshal(product2)
 	
 			if err != nil {
@@ -202,6 +204,45 @@ func productByIdHandler(db *sql.DB) http.HandlerFunc {
 			w.WriteHeader(http.StatusOK)
 			w.Write(jData)
 		}
+	}
+}
+
+func userHandler(db *sql.DB) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method == http.MethodGet {
+			log.Println("Error with user, No GET allowed.")
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var user users
+
+		err := json.NewDecoder(r.Body).Decode(&user)
+
+		if err != nil {
+			http.Error(w, "User info error", http.StatusBadRequest)
+			return
+		}
+
+		securePassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+
+		if err != nil {
+			log.Println("Bcrypting password did not work")
+			http.Error(w, "Bcrypt Error.", http.StatusExpectationFailed)
+			return
+		}
+
+
+		_, err2 := db.Exec("INSERT INTO usersdb(username, email, password) VALUES ($1, $2, $3)", user.Username, user.Email, securePassword)
+
+		if err2 != nil {
+			log.Println("Error with insert user into db", err2)
+			http.Error(w, "User could not be added to db", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Printf("User %s has been added.", user.Username)
 	}
 }
 
@@ -231,10 +272,9 @@ func main(){
 
 	http.HandleFunc("/api", mainPageHandler(db))
 	http.HandleFunc("/api/product/", productByIdHandler(db))
+	http.HandleFunc("/api/users/", userHandler(db))
 
 	fmt.Println("Listening on Port 3000")
 	http.ListenAndServe(":3000", nil)
 	
-
-
 }	
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 	rsc.io/sampler v1.3.0 // indirect
 )
